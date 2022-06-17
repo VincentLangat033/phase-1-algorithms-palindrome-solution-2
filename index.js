ec67afaae66b9e6e8aacfa82c1b921e63866a521
Vincent Langat <kimutailangat033@gmail.com>
2022-06-17 23:10:10 +0300

    commit tree a52dbf81f7c03e6db7188391f051b5abc7944edb

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   for(i=0; i<word.length /2; i++){
+//   const j = word.length -1 - i;
+//   const startChar= word[i]
+//   const endChar = word[j]
+//   if (startChar!== endChar) return false;
+// }
+// return true;
+// }
 
 /* 
   Add your pseudocode here
@@ -12,9 +30,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
